d6184b22f427e6a9130690c35c8df55888079866
1am1337 <oskar.nitzschker@icloud.com>
2026-02-12 11:56:58 +0100

    fixed merge errors upsi

--- scripts/search.js
+++ scripts/search.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-function searchEcosia() {
-    const query = document.getElementById('searchInput').value;
-    const url = `https://www.ecosia.org/search?method=index&q=${encodeURIComponent(query)}`;
-=======
+
 function fireSearch() {
     const searchEngine = document.getElementById('searchInput').placeholder;
     if (searchEngine === "Search DuckDuckGo...") {
@@ -25,17 +21,12 @@ function fireSearch() {
 function searchDuckDuckGo() {
     const query = document.getElementById('searchInput').value;
     const url = `https://noai.duckduckgo.com/?q=${encodeURIComponent(query)}`;
->>>>>>> f91409453251ba191a85d3ac33b3f7b63c9a1877
     window.location.href = url;
 }
 
 document.addEventListener('keydown', function(event) {  
     if(event.key == 'Enter') {
-<<<<<<< HEAD
-        searchEcosia();
-=======
         fireSearch();
->>>>>>> f91409453251ba191a85d3ac33b3f7b63c9a1877
     }
 });
 
